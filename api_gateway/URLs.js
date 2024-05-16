78875cf013d3879982e39b0f511ba4789714052c
asimsinan <asimyuksel@sdu.edu.tr>
2024-05-16 15:48:32 +0300

    fix

--- api_gateway/URLs.js
+++ api_gateway/URLs.js
@@ -1,4 +1,4 @@
 module.exports = {
-    SIPARISLER_API_URL: 'https://siparislerservis.vercel.app/siparisler',
-    URUNLER_API_URL: 'https://urunlerservis.vercel.app/urunler'
+    SIPARISLER_API_URL: 'https://siparislerservis.vercel.app',
+    URUNLER_API_URL: 'https://urunlerservis.vercel.app'
   };
